7dfd3eeeafadb8b8675cebbbe71cdfbb9d93bdd7
Noel Mendoza <nmendozavd@gmail.com>
2018-02-26 18:31:34 -0800

    initial commit

--- Adding to Beasts/ViewController.swift
+++ Adding to Beasts/ViewController.swift
@@ -8,18 +8,64 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var wordArray:[String] = []
+    
+    @IBOutlet var myTableView: UITableView!
+    @IBOutlet var myTextField: UITextField!
+        
+    // Button pressed
+    
+    @IBAction func beastButtonPressed(_ sender: UIButton) {
+        if myTextField.text == nil {
+            return
+        }
+        
+        if let newWord = myTextField.text {
+            
+            wordArray.append(newWord)
+            myTableView.reloadData()
+        }
+        else {
+            return
+        }
+    
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        myTableView.delegate = self
+        myTableView.dataSource = self
     }
-
+    
+    // Protocol and Delegate Methods
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return wordArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = myTableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
+        cell.textLabel?.text = wordArray[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        wordArray.remove(at: indexPath.row)
+        myTableView.reloadData()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
+
 
